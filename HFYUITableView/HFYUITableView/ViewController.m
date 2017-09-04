c69eadc76c447e7a0857c599369561487768a938
HanFuyuan <31496066+HanFuyuan@users.noreply.github.com>
2017-09-04 10:53:56 +0800

    添加UITableView
    添加UITableView

--- HFYUITableView/HFYUITableView/ViewController.m
+++ HFYUITableView/HFYUITableView/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *_tableView;
+    NSArray *_jsonArray;
+}
 
 @end
 
@@ -16,7 +20,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+        _tableView.backgroundColor = [UIColor whiteColor];
+        _tableView.rowHeight = 64.0f;
+        [self.view addSubview:_tableView];
+    }
+    [self getJsonData];
+}
+
+- (void)getJsonData
+{
+    _jsonArray = @[@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！",@"你好！",@"哈哈哈",@"Let is go!",@"干什么？",@"来来来。",@"嘿嘿！"];
+    [_tableView reloadData];
+}
+
+#pragma mark -- UITableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_jsonArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static  NSString *CellIdentifier = @"CellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil){
+        cell = [[UITableViewCell alloc]  initWithStyle:UITableViewCellStyleDefault
+                                     reuseIdentifier:CellIdentifier];
+    }else{
+        for (UIView *subView in cell.contentView.subviews) {
+            [subView removeFromSuperview];
+        }
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    cell.textLabel.text = [_jsonArray objectAtIndex:indexPath.row];
+    cell.textLabel.textColor = [UIColor greenColor];
 }
 
 
